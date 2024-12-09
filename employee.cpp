e691286c521dca362283ba8f7cf919362673bab7
Abdelrahman Bahig <23p0390@eng.asu.edu.eg>
2024-12-09 16:30:20 +0200

    init

---
+++ employee.cpp
@@ -0,0 +1,32 @@
+#ifndef EmpolyeeNode_hpp
+#define EmpolyeeNode_hpp
+
+#include <string>
+using namespace std;
+class EmployeeNode
+{
+private:
+    int age;
+    string name;
+    double salary;
+    int workHours;
+    int
+
+public:
+    EmployeeNode(/* args */);
+    ~EmployeeNode();
+};
+
+EmployeeNode::EmployeeNode(/* args */)
+{
+}
+
+EmployeeNode::~EmployeeNode()
+{
+}
+
+
+
+
+
+#endif
