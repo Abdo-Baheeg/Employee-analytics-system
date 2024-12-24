11b12d261e797bb651b8b84698f66642e93cf2be
mohammed72270 <167139172+mohammed72270@users.noreply.github.com>
2024-12-24 20:34:21 +0200

    bst_youssef

--- BST.cpp
+++ BST.cpp
@@ -1,109 +1,3 @@
-#include "BST.hpp"
-#include <iostream>
-using namespace std;
-
-
-template <typename datatype>
-inline bool BST< datatype>::empty(){return root==0;}
-
-template <typename datatype>
-bool  BST< datatype>::searchAux(BST<datatype>::NodePtr& currentptr,const datatype& value)
-{
-    if(currentptr == nullptr)    //empty tree
-        return false;
-
-    if(currentptr -> data < value)
-        return searchAux(currentptr -> right, value);    //go to right subtree
-
-    else if(currentptr -> data > value)
-        return searchAux(currentptr -> left, value);    //go to left subtree
-
-    else
-        return true;      //this is the item
-}
-
-template <typename datatype>
-bool BST< datatype>:: search( datatype value)
-{
-     return searchAux(currentptr,value);
-}
-
-template <typename datatype>
-void BST<datatype>::insertAux(BST<datatype>::NodePtr& root, const datatype& value)
-{
-    if(root == nullptr)        //empty tree
-        root = new BST<datatype>:: NodePtr(value);
-
-    if(value < root->data)
-        insertAux(root->left, value);
-
-    else if(value > root->data)
-        insertAux(root->right, value);
-
-    else
-        cerr<<"Item already exists in BST" <<endl;
-}
-
-template <typename datatype>
-void BST<datatype>::insert (const datatype& value)
-{
-    return insertAux(root,value);
-}
-
-template <typename datatype>
-void BST<datatype>::erase(const datatype& item) {
-
-    bool found;
-    BST<datatype>::BNodeptr deletednode, parent;
-    serachAux(parent,value);
-
-    if (!found) {
-        cout << "value not found \n";
-        return;
-    }
-
-    if (deletednode->left != 0 && deletednode->right != 0) {
-
-
-        BST<datatype>::BinNodePointer deletednodeSuc = deletednode->right;
-        parent = deletednode;
-        while (deletednodeSuc->left != 0) {
-            parent = deletednodeSuc;
-            deletednodeSuc = deletednodeSuc->left;
-        }
-
-        deletednode->data = deletednodeSuc->data;
-        deletednode = deletednodeSuc;
-    }
-
-
-    BST<datatype>::BinNodePointer subtree = deletednode->left;
-    if (subtree == 0) subtree = deletednode->right;
-
-
-    if (parent == 0) myRoot = subtree;
-
-    else if (parent->left == deletednode) parent->left = subtree;
-
-    else parent->right = subtree;
-    delete deletednode;
-}
-
-
-
-template <typename datatype>
-void BST< datatype>:: inorder(ostream&out)
-{
-
-}
-
-template <typename datatype>
-void  BST< datatype>::preorder(ostream&out)
-{
-
-}
-
-template <typename datatype>
 void BST< datatype>::postorder(ostream&out)
 {
 
@@ -113,24 +7,161 @@ template <typename datatype>
 datatype BST< datatype>:: minValue(BNodeptr& root)
 {
 
-   while(root!=nullptr)
+    while(root!=nullptr)
     {
-    searchAux(root->left,value);
+        searchAux(root->left,value);
+    }
+    return value;
+
+
+}
+
+template <typename datatype>
+datatype BST< datatype>::maxValue(BNodeptr& root)
+{
+    while(root!=nullptr)
+    {
+        searchAux(root->right,value);
     }
     return value;
 
 }
 
-template <typename datatype>
-datatype BST< datatype>::maxValue(NodePtr& root){
-    NodePtr temp=root;
-    if(root!=nullptr){
-      while(temp->right!=nullptr){
-        temp=temp->right;
-      }
-      return temp->data;
-    }
-}
+
+//==================================================================
+//#include "BST.hpp"
+//#include <iostream>
+//using namespace std;
+//
+//
+//template <typename datatype>
+//inline bool BST< datatype>::empty(){return root==0;}
+//
+//template <typename datatype>
+//bool  BST< datatype>::searchAux(BST<datatype>::NodePtr& currentptr,const datatype& value)
+//{
+//    if(currentptr == nullptr)    //empty tree
+//        return false;
+//
+//    if(currentptr -> data < value)
+//        return searchAux(currentptr -> right, value);    //go to right subtree
+//
+//    else if(currentptr -> data > value)
+//        return searchAux(currentptr -> left, value);    //go to left subtree
+//
+//    else
+//        return true;      //this is the item
+//}
+//
+//template <typename datatype>
+//bool BST< datatype>:: search( datatype value)
+//{
+//     return searchAux(currentptr,value);
+//}
+//
+//template <typename datatype>
+//void BST<datatype>::insertAux(BST<datatype>::NodePtr& root, const datatype& value)
+//{
+//    if(root == nullptr)        //empty tree
+//        root = new BST<datatype>:: NodePtr(value);
+//
+//    if(value < root->data)
+//        insertAux(root->left, value);
+//
+//    else if(value > root->data)
+//        insertAux(root->right, value);
+//
+//    else
+//        cerr<<"Item already exists in BST" <<endl;
+//}
+//
+//template <typename datatype>
+//void BST<datatype>::insert (const datatype& value)
+//{
+//    return insertAux(root,value);
+//}
+//
+//template <typename datatype>
+//void BST<datatype>::erase(const datatype& item) {
+//
+//    bool found;
+//    BST<datatype>::BNodeptr deletednode, parent;
+//    serachAux(parent,value);
+//
+//    if (!found) {
+//        cout << "value not found \n";
+//        return;
+//    }
+//
+//    if (deletednode->left != 0 && deletednode->right != 0) {
+//
+//
+//        BST<datatype>::BinNodePointer deletednodeSuc = deletednode->right;
+//        parent = deletednode;
+//        while (deletednodeSuc->left != 0) {
+//            parent = deletednodeSuc;
+//            deletednodeSuc = deletednodeSuc->left;
+//        }
+//
+//        deletednode->data = deletednodeSuc->data;
+//        deletednode = deletednodeSuc;
+//    }
+//
+//
+//    BST<datatype>::BinNodePointer subtree = deletednode->left;
+//    if (subtree == 0) subtree = deletednode->right;
+//
+//
+//    if (parent == 0) myRoot = subtree;
+//
+//    else if (parent->left == deletednode) parent->left = subtree;
+//
+//    else parent->right = subtree;
+//    delete deletednode;
+//}
+//
+//
+//
+//template <typename datatype>
+//void BST< datatype>:: inorder(ostream&out)
+//{
+//
+//}
+//
+//template <typename datatype>
+//void  BST< datatype>::preorder(ostream&out)
+//{
+//
+//}
+//
+//template <typename datatype>
+//void BST< datatype>::postorder(ostream&out)
+//{
+//
+//}
+//
+//template <typename datatype>
+//datatype BST< datatype>:: minValue(BNodeptr& root)
+//{
+//
+//   while(root!=nullptr)
+//    {
+//    searchAux(root->left,value);
+//    }
+//    return value;
+//
+//}
+//
+//template <typename datatype>
+//datatype BST< datatype>::maxValue(NodePtr& root){
+//    NodePtr temp=root;
+//    if(root!=nullptr){
+//      while(temp->right!=nullptr){
+//        temp=temp->right;
+//      }
+//      return temp->data;
+//    }
+//}
 
 
 
--- BST.hpp
+++ BST.hpp
@@ -4,45 +4,96 @@
 using namespace std;
 
 template <typename datatype>
-class BST{
+class BST
+{
 public:
     BST(){};
     bool empty();
-    bool search(const datatype& value) const;
-    void insert(const datatype& value);
-    void erase(const datatype& value);
-    void inorder(ostream&out) const;
-    void preorder(ostream&out) const;
-    void postorder(ostream&out) const;
+    void insert( datatype value);
+    void erase( datatype &value);
+    bool search( datatype value);
+    void inorder(ostream&out);
+    void preorder(ostream&out);
+    void postorder(ostream&out);
+
+
 
 private:
- class EmployeeNode{
+    class BNode
+    {
     public:
         datatype data;
-        int age;           // Age of the employee
-        double income;         // Monthly income
-        double performanceScore;  // Performance score
-        EmployeeNode* left;
-        EmployeeNode* right;
-        EmployeeNode(datatype value): data(value),left(nullptr),right(nullptr){};
-        EmployeeNode():left(nullptr),right(nullptr){};
-};
-typedef EmployeeNode* NodePtr; ;
-NodePtr root{};
+        BNode *left;
+        BNode *right;
+        BNode(datatype value): data(value),left(nullptr),right(nullptr){};
+        BNode():left(nullptr),right(nullptr){};
+
+    };typedef*BNodeptr;
+    BNodeptr root;
 
 
 
-void insertAux(BST<datatype>::NodePtr &root, datatype& value);
-bool searchAux(BST<datatype>::NodePtr &root, datatype& value);
-void inorderAux(ostream& out, BST<datatype>::NodePtr subtreePtr);
-void preorderAux(ostream& out, BST<datatype>::NodePtr subtreePtr);
-void postorderAux(ostream& out, BST<datatype>::NodePtr subtreePtr);
+    void  insertAux(BST<datatype>::BNodeptr &root, datatype &value);
+    bool searchAux(BST<datatype>::BNodeptr &root, datatype& value);
+    void inorderAux(ostream& out, BST<datatype>::BNodeptr subtreePtr);
+    void preorderAux(ostream& out, BST<datatype>::BNodeptr subtreePtr);
+    void postorderAux(ostream& out, BST<datatype>::BNodeptr subtreePtr);
 
-datatype minValue(NodePtr& root);
-datatype maxValue(NodePtr& root);
+    datatype minValue(BNodeptr& root);
+    datatype maxValue(BNodeptr& root);
 
 
 
 };
 
 #endif BST_H
+
+//=============================================================
+//#ifndef BST_H
+//#define BST_H
+//#include <iostream>
+//using namespace std;
+//
+//template <typename datatype>
+//class BST{
+//public:
+//    BST(){};
+//    bool empty();
+//    bool search(const datatype& value) const;
+//    void insert(const datatype& value);
+//    void erase(const datatype& value);
+//    void inorder(ostream&out) const;
+//    void preorder(ostream&out) const;
+//    void postorder(ostream&out) const;
+//
+//private:
+// class EmployeeNode{
+//    public:
+//        datatype data;
+//        int age;           // Age of the employee
+//        double income;         // Monthly income
+//        double performanceScore;  // Performance score
+//        EmployeeNode* left;
+//        EmployeeNode* right;
+//        EmployeeNode(datatype value): data(value),left(nullptr),right(nullptr){};
+//        EmployeeNode():left(nullptr),right(nullptr){};
+//};
+//typedef EmployeeNode* NodePtr; ;
+//NodePtr root{};
+//
+//
+//
+//void insertAux(BST<datatype>::NodePtr &root, datatype& value);
+//bool searchAux(BST<datatype>::NodePtr &root, datatype& value);
+//void inorderAux(ostream& out, BST<datatype>::NodePtr subtreePtr);
+//void preorderAux(ostream& out, BST<datatype>::NodePtr subtreePtr);
+//void postorderAux(ostream& out, BST<datatype>::NodePtr subtreePtr);
+//
+//datatype minValue(NodePtr& root);
+//datatype maxValue(NodePtr& root);
+//
+//
+//
+//};
+//
+//#endif BST_H
